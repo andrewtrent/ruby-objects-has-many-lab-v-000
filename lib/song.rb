e32aa3aaa7bf81830e8d2d0557c93b0d3fc70990
Andrew Trent <andrewtrentdesign@gmail.com>
2018-11-05 22:45:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,10 +13,10 @@ class Song
   end
 
   def artist_name
-    if self.artist.name
-      self.artist.name
-    else
+    if self.artist == nil
       nil
+    else
+      self.artist.name
     end
   end
 
