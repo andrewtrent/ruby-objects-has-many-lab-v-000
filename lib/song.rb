68a0c69712c2b2d882789697fc6ef7330f6f5eac
Andrew Trent <andrewtrentdesign@gmail.com>
2018-11-05 22:39:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,17 @@
 class Song
   attr_accessor :name, :artist
+  
   @@all = []
+
   def initialize(name)
     @name = name
     @@all << self
   end
 
+  def self.all
+    @@all 
+  end 
+  
   def artist_name
     if true
       self.artist.name
