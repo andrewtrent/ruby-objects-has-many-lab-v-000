ffbc45c8289a1791b26934469dba5f5d4daf5aa2
Andrew Trent <andrewtrentdesign@gmail.com>
2018-11-05 22:26:15 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  attr_accessor :name
+  @@song_count = 0
+
+  @songs = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_song(song)
+    @songs << song
+    @@song_count += 1
+  end
+
+  def add_song_by_name(name)
+    add_song(Song.new(name))
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+end
+
+    
